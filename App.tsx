500c89cd10a68bcfe751e0d01d1cdfd7564843d5
Workbook0 <workbook0@Workbook0s-MacBook-Pro.local>
2020-01-22 15:22:09 -0800

    sets up typescript compilation with webpack

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+import { Header } from "./src/components/Header";
+
+export default () => (
+    <Header />
+)
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export const Header = () => (
+    <header>
+        <h2>Header</h2>
+        
+    </header>
+)
