7377acf77ecbf24c16cee3e161469067d7dc503a
Ryan Cahela <ryancahela@gmail.com>
2020-01-22 16:15:31 -0800

    adds unordered list to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,8 @@ import * as React from "react";
 export const Header = () => (
     <header>
         <h2>Header</h2>
-        
+        <ul>
+            <li>Item 1</li>
+        </ul>
     </header>
 )
